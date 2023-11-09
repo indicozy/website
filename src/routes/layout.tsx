1e6fd726921b9d08a4c85443494071ba4bf53a66
indicozy <berkutkarlibai@gmail.com>
2023-11-09 21:15:46 +0600

    ughhhh

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -11,12 +11,14 @@ import styles from "./styles.css?inline";
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
   // https://qwik.builder.io/docs/caching/
-  cacheControl({
-    // Always serve a cached response by default, up to a week stale
-    staleWhileRevalidate: 60 * 60 * 24 * 7,
-    // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
-    maxAge: 60 * 60,
-  });
+  cacheControl(
+    {
+      public: true,
+      staleWhileRevalidate: 60 * 60 * 24 * 7,
+      maxAge: 60 * 60,
+    },
+    "Cloudflare-CDN-Cache-Control"
+  );
 };
 
 export const useServerTimeLoader = routeLoader$(() => {
