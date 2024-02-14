2fccf9c9d1198bde6499f5ba9705f37f3b7fd9ac
indicozy <berkutkarlibai@gmail.com>
2024-02-14 11:41:05 +0600

    init

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -1,4 +1,4 @@
-import { component$, useSignal } from "@builder.io/qwik";
+import { $, component$, useOn, useOnWindow, useSignal } from "@builder.io/qwik";
 // import { QwikLogo } from "../icons/qwik";
 import styles from "./header.module.css";
 import { Link, useLocation } from "@builder.io/qwik-city";
@@ -20,19 +20,13 @@ const HeaderItem = component$<{
   const url = useLocation();
 
   const checkedUrl = useSignal(`/${url.url.pathname.slice(1, -1)}`);
-  const isChecked = checkedUrl.value === `/${href}`
+  const isChecked = checkedUrl.value === `/${href}`;
 
   return (
     <>
       <input type="radio" id={id} name="tabs" checked={isChecked} />
-      <Link
-        prefetch
-        href={hrefAbsolute}
-      >
-        <label
-          class={styles.tab}
-          for={id}
-        >
+      <Link prefetch href={hrefAbsolute}>
+        <label class={styles.tab} for={id}>
           {text}
         </label>
       </Link>
@@ -42,25 +36,22 @@ const HeaderItem = component$<{
 
 // NOTE: Qwik does not support persisting state during route changes
 const LinksHandler = component$<{ links: TLink[] }>(({ links }) => {
-  const linksAggregate = links.map((l) => (
-    <HeaderItem
-      key={l.href}
-      link={l}
-    />
-  ));
-  const isOpaque = useSignal<boolean>(false); // TODO: bug, false if browser loaded from scrolled position
+  const linksAggregate = links.map((l) => <HeaderItem key={l.href} link={l} />);
+  const isOpaque = useSignal<boolean>(false);
+  useOnWindow(
+    "scroll",
+    $(() => {
+      isOpaque.value = window.scrollY > 50;
+    })
+  );
+  useOn(
+    "load",
+    $(() => {
+      isOpaque.value = window.scrollY > 50;
+    })
+  );
   return (
-    <div
-      document:onscroll$={() => {
-        const y = window.scrollY;
-        if (y > 50) {
-          isOpaque.value = true;
-        } else {
-          isOpaque.value = false;
-        }
-      }}
-      class={[styles.tabs, isOpaque.value ? styles.tabs_opaque : null]}
-    >
+    <div class={[styles.tabs, isOpaque.value ? styles.tabs_opaque : null]}>
       {linksAggregate}
       <span class={styles.glider}></span>
     </div>
